df4a8dc0d72c12ea6154dd44466ced82162f323c
Frank Glynne Grobbelaar <s209022019@Mandela.ac.za>
2021-09-13 16:59:51 +0200

    fixing user logged in issue

---
+++ WebApp_ExcelFileProcessor/Controllers/BaseClassController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_ExcelFileProcessor.Data;
+
+namespace WebApp_ExcelFileProcessor.Controllers
+{
+    public class BaseClassController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public BaseClassController(ILogger<HomeController> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult UploadBaseClass()
+        {
+            return View();
+        }
+        public IActionResult ManageBaseClass()
+        {
+            return View();
+        }
+    }
+}
--- WebApp_ExcelFileProcessor/Controllers/DashboardController.cs
+++ WebApp_ExcelFileProcessor/Controllers/DashboardController.cs
@@ -8,18 +8,37 @@ namespace WebApp_ExcelFileProcessor.Controllers
     [Authorize]
     public class DashboardController : Controller
     {
-        private readonly ILogger<DashboardController> _logger;
+        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
 
-        public DashboardController(ILogger<DashboardController> logger, UserManager<IdentityUser> userManager)
+        public DashboardController(ILogger<HomeController> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
-            _userManager = userManager;
             _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
+        }
+        [Authorize]
+        public IActionResult UploadBaseClass()
+        {          
+            return RedirectToAction("UploadBaseClass", "BaseClass");
+        }
+        [Authorize]
+        public IActionResult ManageBaseClass()
+        {           
+            return RedirectToAction("ManageBaseClass", "BaseClass");
+        }
+        [Authorize]
+        public IActionResult Signout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
